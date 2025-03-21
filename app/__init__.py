d3cf4b43d5dc115d6c68d260bdeefd24b2cee6b7
RoySamson-stack <rsam9009@gmail.com>
2025-03-21 19:45:27 +0300

    updating thw project structure

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+
+# Initialize extensions
+db = SQLAlchemy()
+jwt = JWTManager()
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_pyfile('config.py')
+
+    db.init_app(app)
+    jwt.init_app(app)
+
+    from .api import api_blueprint
+    app.register_blueprint(api_blueprint, url_prefix='/api')
+
+    return app
